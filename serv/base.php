8126621ec27592433a60660de2ebf4cfa18e8471
Vladimir <shues@bk.ru>
2016-07-22 13:19:53 +0300

    Base.php  теперь более универсален, а css теперь не подсвечивает дату в таблице расходов

--- serv/base.php
+++ serv/base.php
@@ -1,7 +1,10 @@
 <?php
 	function dbconn() {
-		//$db = mysqli_connect("mysql.hostinger.ru", "u423387145_crosh", "glos2ar12", "u423387145_money");
-		$db = mysqli_connect("localhost", "root", "glos2ar12", "money");
+        $db = mysqli_connect("localhost", "root", "glos2ar12", "money");		
+        if(!db){
+            $db = mysqli_connect("mysql.hostinger.ru", "u423387145_crosh", "glos2ar12", "u423387145_money");
+        }
+		
 		return $db;
 	}
 ?>
