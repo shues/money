0709a9d19bc414ceb905226582118a5aeeb57b9b
Vladimir <shues@bk.ru>
2016-07-22 13:42:59 +0300

    таки угробил base.php

--- serv/base.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function dbconn() {
-        $db = mysqli_connect("localhost", "root", "glos2ar12", "money");		
-        //$db = mysqli_connect("mysql.hostinger.ru", "u423387145_crosh", "glos2ar12", "u423387145_money");	
-		return $db;
-	}
-?>
